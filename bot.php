39cef7e9f6e1157fbfc1fd909a3f24923f133ccc
Nasrullox01 <122689051+Nasrullox01@users.noreply.github.com>
2023-01-14 22:25:52 +0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,11 +7,19 @@
     // echo $chat_id;
     $text = $telegram->Text();
      
-         
-     
-   if($text == "/start")
-   {
-    $content = array('chat_id' => $chat_id, 'text' => "Salom hush kelibsiz!"); 
+$telegram = new Telegram('YOUR TELEGRAM TOKEN HERE');
+
+$req = $telegram->getUpdates();
+
+for ($i = 0; $i < $telegram-> UpdateCount(); $i++) {
+	// You NEED to call serveUpdate before accessing the values of message in Telegram Class
+	$telegram->serveUpdate($i);
+	$text = $telegram->Text();
+	$chat_id = $telegram->ChatID();
+
+	if ($text == '/start') {
+		$reply = 'Working';
+	 $content = array('chat_id' => $chat_id, 'text' => "Salom hush kelibsiz!"); 
     $telegram->sendMessage($content); 
     
      $ulash = mysqli_connect("localhost","id17907369_rootbot","l(Gysg/1@W*#E7}F","id17907369_itedukokandbot") or die("Bazaga ulanmadi");
@@ -59,7 +67,9 @@
     $keyb = $telegram->buildKeyBoard($option, $onetime=false, $resize=true);
     $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "This is a Keyboard Test");
     $telegram->sendMessage($content);
-    }   
+	}
+	// DO OTHER STUFF
+}  
     
     
     
